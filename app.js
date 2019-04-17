ffae60b19e7067f67f19ec15446fdb3087023509
Kyle Monahan <kyle.monahan2000@gmail.com>
2019-04-17 10:13:34 -0500

    removed console log

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 $( document ).ready(function() {
  
   $.getJSON( 'page-1.json', function( json ) {
-    json.forEach(element => {
-      console.log( element.image_url);
-
-      
-    });
     json.forEach(element =>{
         let newImg = $('<img id="dynamic">');
         newImg.attr('src', element.image_url);
