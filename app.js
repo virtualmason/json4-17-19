376588942419e5c1f9e4da6e47b97d8b35c9b589
Kyle Monahan <kyle.monahan2000@gmail.com>
2019-04-17 13:14:40 -0500

    finished filter

--- app.js
+++ app.js
@@ -15,63 +15,16 @@ $( document ).ready(function() {
       let img = new NewImage(element.image_url, element.title, element.description, element.keyword, element.horns);
       let newImg = $('<img id="dynamic">');
       newImg.attr('src', element.image_url);
+      newImg.attr('alt', element.keyword);
       newImg.appendTo('#photo-template');
       list.push(img);
 
     });
   });
 
-  // function filter(check){
   $('#mySelect').change(function(){
     var value = $(this).val();
-
-    for(var i =0; i < list.length; i++) {
-      if(value !== list[i].keyword) {
-
-        list[i].css();
-      }
-
-      //   $(`${list[i]}`).hide();
-      // } else {
-      //   $('list[i].keyword').show();          }
-      // var currentDivData = list[i].getAttribute('data-bird');
-      // if(check.id === 'all') {
-      //   location.reload();
-      // }
-      // if(currentDivData !== check.id) {
-      //   dataList[i].classList.add('hide');
-      // } else {
-      //   dataList[i].classList.remove('hide');
-      // }
-    }
-
+        $(`img`).hide();
+        $(`img[alt=${value}]`).show();
   });
-
-  // if value selected === item in list
-  //if(currentDivData !== check.id) {
-  //     dataList[i].classList.add('hide');
-  //   } else {
-  //     dataList[i].classList.remove('hide');
-  //   }
-
-  // var dataList = document.querySelectorAll('[data-bird]');option[value=two]
-  // $( "option[value]" );
-  // console.log();
-  // for(var i =0; i < dataList.length; i++) {
-  //   var currentDivData = dataList[i].getAttribute('data-bird');
-  //   if(check.id === 'all') {
-  //     location.reload();
-  //   }
-  //   if(currentDivData !== check.id) {
-  //     dataList[i].classList.add('hide');
-  //   } else {
-  //     dataList[i].classList.remove('hide');
-  //   }
-  // }
-
-
-
-
-
-
 });
