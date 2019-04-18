b3e9f93492047a5b054c66156085647c89bbbf97
Floyd Orr <floydcanhelp@gmail.com>
2019-04-18 12:45:52 -0500

    handlebar working

--- app.js
+++ app.js
@@ -1,29 +1,18 @@
 $( document ).ready(function() {
- 
 
-  function NewImage(image_url, title, description, keyword, horns) {
-    this.image_url = image_url;
-    this.title = title;
-    this.description = description;
-    this.keyword = keyword;
-    this.horns = horns;
-  }
   let uniqueArr =[];
   $.getJSON( 'page-1.json', function( json ) {
     json.forEach(element =>{
       if(!uniqueArr.includes(element.keyword)) {
         uniqueArr.push(element.keyword);
       }
-      let img = new NewImage(element.image_url, element.title, element.description, element.keyword, element.horns);
-      let imgTitle = $(`<h2 id="${element.keyword}">${element.title}</h2>`);
-      imgTitle.appendTo('#photo-template');
-      let newImg = $('<img id="dynamic">');
-      newImg.attr('src', element.image_url);
-      newImg.attr('alt', element.keyword);
-      newImg.appendTo('#photo-template');
-      let imgDescription = $(`<p id="${element.keyword}">${element.description}</p>`);
-      imgDescription.appendTo('#photo-template');
-      // list.push(img);
+    
+      console.log('img form handlebars', element.image_url);
+      var source   = document.getElementById('entry-template').innerHTML;
+      var template = Handlebars.compile(source);
+      var context = {id: element.keyword,title: element.keyword, description: element.description, image:element.image_url, alt:element.keyword};
+      var html    = template(context);
+      $('body').append(html);
 
     });
 
@@ -47,10 +36,5 @@ $( document ).ready(function() {
     }
 
   });
-  // Handle bars
-  var source   = document.getElementById('entry-template').innerHTML;
-  var template = Handlebars.compile(source);
-  var context = {title: 'My New Post', body: 'This is my first post!'};
-  var html    = template(context);
-  $('body').append(html);
+ 
 });
